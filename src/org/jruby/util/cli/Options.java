99024041577fccc4da6d0aaae5a124fda7bf90e6
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-05-28 22:16:38 +0300

    Options added

--- src/org/jruby/util/cli/Options.java
+++ src/org/jruby/util/cli/Options.java
@@ -111,6 +111,7 @@ public class Options {
     public static final Option<Boolean> IR_DEBUG             = bool(IR, "ir.debug", false, "Debug generation of JRuby IR.");
     public static final Option<Boolean> IR_PROFILE           = bool(IR, "ir.profile", false, "[EXPT]: Profile IR code during interpretation.");
     public static final Option<Boolean> IR_COMPILER_DEBUG    = bool(IR, "ir.compiler.debug", false, "Debug compilation of JRuby IR.");
+    public static final Option<Boolean> IR_PERSISTENCE       = bool(IR, "ir.persistence", false, "Persist generation of JRuby IR.");
     public static final Option<String>  IR_COMPILER_PASSES = string(IR, "ir.passes", null, null, "Specify comma delimeted list of passes to run.");
     public static final Option<String>  IR_INLINE_COMPILER_PASSES = string(IR, "ir.inline_passes", null, null, "Specify comma delimeted list of passes to run after inlining a method.");
     
