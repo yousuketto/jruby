acc3f159333126a10ac23a1a5497e61264f5563c
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-06-14 01:57:08 +0300

    Initial work on IR reading back from file

--- src/org/jruby/ir/IRTranslator.java
+++ src/org/jruby/ir/IRTranslator.java
@@ -8,31 +8,52 @@ import org.jruby.ir.persistence.IRPersistenceException;
 import org.jruby.ir.persistence.IRPersistenceFacade;
 
 /**
- * Abstract class that contains general logic for both IR Compiler and IR Interpreter
- *
- * @param <R> type of returned object by translator
- * @param <S> type of specific for translator object
+ * Abstract class that contains general logic for both IR Compiler and IR
+ * Interpreter
+ * 
+ * @param <R>
+ *            type of returned object by translator
+ * @param <S>
+ *            type of specific for translator object
  */
 public abstract class IRTranslator<R, S> {
-    
-    public R performTranslation(Ruby runtime, Node node, S specificObject) {        
-        IRScope producedIRScope = produceIrScope(runtime, node);
-        
-        if(isIRPersistenceRequired()) {
-            try {
-                IRPersistenceFacade.persist(producedIRScope, runtime);
-            } catch (IRPersistenceException e) {
-                // FIXME: Log error, but do not interrupt translation 
-                // Or should we throw runtime exception?
-                e.printStackTrace();
-            }
+
+    public R performTranslation(Ruby runtime, Node node, S specificObject) {
+
+        IRScope producedIRScope = null;
+        if (isIRPersistenceRequired()) {
+            producedIRScope = irPersistenseSpecificLogic(runtime, node, producedIRScope);
+        } else {
+            producedIRScope = produceIrScope(runtime, node);
         }
-        
+
         R result = translationSpecificLogic(runtime, producedIRScope, specificObject);
         return result;
     }
-    
-    protected abstract R translationSpecificLogic(Ruby runtime, IRScope producedIrScope, S specificObject);
+
+    private IRScope irPersistenseSpecificLogic(Ruby runtime, Node node, IRScope producedIRScope) {
+        try {
+            // if IR is already persisted
+            if (IRPersistenceFacade.isPersistedIrExecution(runtime)) {                    
+                producedIRScope = IRPersistenceFacade.read(runtime);
+            } else { // produce ir and persist it
+                producedIRScope = produceIrScope(runtime, node);
+                IRPersistenceFacade.persist(producedIRScope, runtime);
+            }
+        } catch (IRPersistenceException e) {
+            // FIXME: Log error, but do not interrupt translation
+            // Or should we throw runtime exception?
+            e.printStackTrace();
+            // Do not interrupt translation
+            if (producedIRScope == null) {
+                producedIRScope = produceIrScope(runtime, node);
+            }
+        }
+        return producedIRScope;
+    }
+
+    protected abstract R translationSpecificLogic(Ruby runtime, IRScope producedIrScope,
+            S specificObject);
 
     private static boolean isIRPersistenceRequired() {
         return RubyInstanceConfig.IR_PERSISTENCE;
@@ -42,9 +63,9 @@ public abstract class IRTranslator<R, S> {
         IRManager irManager = runtime.getIRManager();
         boolean is1_9 = runtime.is1_9();
         IRBuilder irBuilder = IRBuilder.createIRBuilder(irManager, is1_9);
-        
+
         final IRScope irScope = irBuilder.buildRoot((RootNode) node);
         return irScope;
-    }    
+    }
 
 }
--- src/org/jruby/ir/persistence/IRPersistenceFacade.java
+++ src/org/jruby/ir/persistence/IRPersistenceFacade.java
@@ -1,29 +1,71 @@
 package org.jruby.ir.persistence;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.jruby.Ruby;
 import org.jruby.RubyInstanceConfig;
 import org.jruby.ir.IRScope;
+import org.jruby.ir.IRScriptBody;
+import org.jruby.ir.instructions.CopyInstr;
+import org.jruby.ir.instructions.ReceiveSelfInstr;
+import org.jruby.ir.operands.CurrentScope;
+import org.jruby.ir.operands.ScopeModule;
 import org.jruby.ir.persistence.util.FileIO;
-import org.jruby.ir.persistence.util.IRFilePathManager;
+import org.jruby.ir.persistence.util.IRFileExpert;
 
+// This class currently contains code that will be decoupled later on 
 public class IRPersistenceFacade {
+    
+    public static boolean isPersistedIrExecution(Ruby runtime) {
+        RubyInstanceConfig config = runtime.getInstanceConfig();
+        String fileName = config.displayedFileName();
+        boolean isIrFileName = IRFileExpert.INSTANCE.isIrFileName(fileName);
+        return isIrFileName || IRFileExpert.INSTANCE.isIrFileForRbFileFound(config);
+    }
 
-    public static void persist(IRScope irScopeToPersist, Ruby runtime) throws IRPersistenceException {
+    public static void persist(IRScope irScopeToPersist, Ruby runtime)
+            throws IRPersistenceException {
         try {
             RubyInstanceConfig config = runtime.getInstanceConfig();
+            File irFile = IRFileExpert.INSTANCE.getIRFileInIntendedPlace(config);
             
-            // Place ir files inside ir folder under current directory
-            File currentDir = new File(config.getCurrentDirectory());
-            String rbFileName = config.displayedFileName();            
-            File irFile = IRFilePathManager.INSTANCE.getIRFile(currentDir, rbFileName);                      
-
             String instructionsString = irScopeToPersist.toStringInstrs();
             FileIO.INSTANCE.writeToFile(irFile, instructionsString);
         } catch (Exception e) {
             throw new IRPersistenceException(e);
         }
 
+    }
+
+    public static IRScope read(Ruby runtime) throws IRPersistenceException {
+        try {
+            RubyInstanceConfig config = runtime.getInstanceConfig();
+            File irFile = IRFileExpert.INSTANCE.getIRFileInIntendedPlace(config);
+            
+            // FIXME: It is copy/paste from IRBuilder#buildRoot, 
+            // maybe we will need to read IR that was not produced by IRBuilder#buildRoot? 
+            IRScriptBody script = new IRScriptBody(runtime.getIRManager(), "__file__", irFile.getAbsolutePath(), null);
+            script.addInstr(new ReceiveSelfInstr(script.getSelf()));
+            // Set %current_scope = <current-scope>
+            // Set %current_module = <current-module>
+            script.addInstr(new CopyInstr(script.getCurrentScopeVariable(), new CurrentScope(script)));
+            script.addInstr(new CopyInstr(script.getCurrentModuleVariable(), new ScopeModule(script)));
+            // end of copy/paste
+            
+            // read line by line and transform every line with instruction into instruction
+            String[] fileLines = FileIO.INSTANCE.readFile(irFile);
+            for (String line : fileLines) {
+                
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 }
