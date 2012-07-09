bfc3033b173aad64b4d8aea404c74c24d52129b3
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-07-09 21:07:20 +0300

    Options for reading added

--- src/org/jruby/util/cli/Options.java
+++ src/org/jruby/util/cli/Options.java
@@ -111,7 +111,8 @@ public class Options {
     public static final Option<Boolean> IR_DEBUG             = bool(IR, "ir.debug", false, "Debug generation of JRuby IR.");
     public static final Option<Boolean> IR_PROFILE           = bool(IR, "ir.profile", false, "[EXPT]: Profile IR code during interpretation.");
     public static final Option<Boolean> IR_COMPILER_DEBUG    = bool(IR, "ir.compiler.debug", false, "Debug compilation of JRuby IR.");
-    public static final Option<Boolean> IR_PERSISTENCE       = bool(IR, "ir.persistence", false, "Persist generation of JRuby IR.");
+    public static final Option<Boolean> IR_PERSISTENCE       = bool(IR, "ir.persistence", false, "Persist generated JRuby IR.");
+    public static final Option<Boolean> IR_READING       = bool(IR, "ir.reading", false, "Read JRuby IR file.");
     public static final Option<String>  IR_COMPILER_PASSES = string(IR, "ir.passes", null, null, "Specify comma delimeted list of passes to run.");
     public static final Option<String>  IR_INLINE_COMPILER_PASSES = string(IR, "ir.inline_passes", null, null, "Specify comma delimeted list of passes to run after inlining a method.");
     
