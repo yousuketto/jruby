cbc677bd60b309886a926baf2e3d95a25c800092
cdwijayarathna <cdwijayarathna@gmail.com>
2013-08-28 06:17:22 +0530

    4 require Probes Added and rake solved

--- core/src/main/java/org/jruby/runtime/load/LoadService.java
+++ core/src/main/java/org/jruby/runtime/load/LoadService.java
@@ -375,6 +375,8 @@ public class LoadService {
     }
 
     public SearchState findFileForLoad(String file) {
+        
+        provider.findRequireEntry(file, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
         if (Platform.IS_WINDOWS) {
             file = file.replace('\\', '/');
         }
@@ -391,21 +393,21 @@ public class LoadService {
         for (LoadSearcher searcher : searchers) {
             if (searcher.shouldTrySearch(state)) {
                 if (!searcher.trySearch(state)) {
+                    provider.findRequireReturn(file, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
                     return null;
                 }
             }
         }
-
+        provider.findRequireEntry(file, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
         return state;
     }
 
     public boolean require(String requireName) {
-        provider.findRequireEntry(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
+        provider.requireEntry(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
         return requireCommon(requireName, true) == RequireState.LOADED;
     }
 
     public boolean autoloadRequire(String requireName) {
-        provider.requireEntry(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
         return requireCommon(requireName, false) != RequireState.CIRCULAR;
     }
 
@@ -416,6 +418,7 @@ public class LoadService {
     private RequireState requireCommon(String requireName, boolean circularRequireWarning) {
         // check for requiredName without extension.
         if (featureAlreadyLoaded(requireName)) {
+            provider.requireReturn(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
             return RequireState.ALREADY_LOADED;
         }
 
@@ -423,6 +426,7 @@ public class LoadService {
             if (circularRequireWarning && runtime.isVerbose()) {
                 warnCircularRequire(requireName);
             }
+            provider.requireReturn(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
             return RequireState.CIRCULAR;
         }
         try {
@@ -432,6 +436,7 @@ public class LoadService {
 
             // check for requiredName again now that we're locked
             if (featureAlreadyLoaded(requireName)) {
+                provider.requireReturn(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
                 return RequireState.ALREADY_LOADED;
             }
 
@@ -439,11 +444,13 @@ public class LoadService {
             long startTime = loadTimer.startLoad(requireName);
             try {
                 boolean loaded = smartLoadInternal(requireName);
+                provider.requireReturn(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
                 return loaded ? RequireState.LOADED : RequireState.ALREADY_LOADED;
             } finally {
                 loadTimer.endLoad(requireName, startTime);
             }
         } finally {
+            //provider.requireReturn(requireName, runtime.getCurrentContext().getFile(), runtime.getCurrentContext().getLine());
             requireLocks.unlock(requireName);
         }
     }
