a64213be33f3bbd4ca0ccb556bcabec82ce932dd
cdwijayarathna <cdwijayarathna@gmail.com>
2013-08-31 21:32:36 +0530

    objectCreate version 1 added

--- core/src/main/java/org/jruby/RubyBasicObject.java
+++ core/src/main/java/org/jruby/RubyBasicObject.java
@@ -50,7 +50,10 @@ import org.jruby.runtime.Helpers;
 import org.jruby.runtime.Block;
 import org.jruby.runtime.CallType;
 import org.jruby.runtime.ClassIndex;
+import org.jruby.runtime.JRuby;
 import org.jruby.runtime.ObjectAllocator;
+import org.jruby.runtime.ObjectSpace;
+import org.jruby.runtime.Provider;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.Visibility;
 import static org.jruby.runtime.Visibility.*;
@@ -106,6 +109,8 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
     private static final Logger LOG = LoggerFactory.getLogger("RubyBasicObject");
 
     private static final boolean DEBUG = false;
+    
+    public static JRuby provider= Provider.getInstance();
     
     /** The class of this object */
     protected transient RubyClass metaClass;
@@ -230,15 +235,18 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      */
     public RubyBasicObject(Ruby runtime, RubyClass metaClass) {
         this.metaClass = metaClass;
-
-        runtime.addToObjectSpace(true, this);
+        runtime.addToObjectSpace(true, this);        
+        provider.objectCreate(metaClass.getName());
+    
     }
+        
 
     /**
      * Path for objects that don't taint and don't enter objectspace.
      */
     public RubyBasicObject(RubyClass metaClass) {
         this.metaClass = metaClass;
+        provider.objectCreate(metaClass.getName());
     }
 
     /**
@@ -247,9 +255,9 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      * considered immediate, they'll always pass false here)
      */
     protected RubyBasicObject(Ruby runtime, RubyClass metaClass, boolean useObjectSpace) {
-        this.metaClass = metaClass;
-
+        this.metaClass = metaClass;        
         runtime.addToObjectSpace(useObjectSpace, this);
+        //provider.objectCreate(metaClass.getName());
     }
 
     protected void taint(Ruby runtime) {
